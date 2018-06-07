fa6bb7177388a9faac7127309cd37be025826829
593170 <39775732+593170@users.noreply.github.com>
2018-06-07 12:42:25 -0400

    Update gameStateManager.java

--- gameStateManager.java
+++ gameStateManager.java
@@ -1,29 +1,20 @@
 import java.awt.Graphics;
 import java.util.Stack;
-
 public class gameStateManager {
 	private Stack<gameState> states;
 	public gameStateManager() {
 		states = new Stack<gameState>();
 	}
-	
 	public void tick() {
 		states.peek().tick();
 	}
-	
 	public void draw(Graphics z) {
 		states.peek().draw(z);
 	}
-	
 	public void keyPressed(int k) {
 		states.peek().keyPressed(k);
 	}
-	
 	public void keyReleased(int k) {
 		states.peek().keyReleased(k);
 	}
-	
-	
-	
-	
 }
