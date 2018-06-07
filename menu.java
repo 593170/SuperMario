85e35207bdd369c3f4bc20dab207d2922955967c
593170 <39775732+593170@users.noreply.github.com>
2018-06-07 12:57:03 -0400

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,21 @@
+import java.awt.Graphics;
+public class menu extends gameState{
+	protected menu(gameStateManager gsm) {
+		super(gsm);
+	}
+	public void init() {
+	
+	}
+	public void tick() {	
+	
+	}
+	public void draw(Graphics Z) {
+			
+	}
+	public void keyPressed(int k) {
+			
+	}
+	public void keyReleased(int k) {
+			
+	}
+}
