477ebf1dab3d3a8fc2871804b597b52cb6ad913c
593170 <39775732+593170@users.noreply.github.com>
2018-06-11 21:23:57 -0400

    Update menuState.java

--- menuState.java
+++ menuState.java
@@ -2,20 +2,15 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
-
 import javax.swing.JFrame;
-
 public class menuState extends gameState {
-	
 	private String[] menuOptions = {"Start", "Quit"};
 	private int selection = 0;
 	protected menuState(gameStateManager gsm) {
 		super(gsm);
 	}
 	public void init() {}
-	
 	public void tick() {	
-	
 	}
 	public void draw(Graphics z) {
 		z.setColor(new Color(0, 0, 0));
@@ -26,13 +21,9 @@ public class menuState extends gameState {
 			} else {
 				z.setColor(Color.white);
 			}
-			
-			
 			z.setFont(new Font("Arial", Font.PLAIN, 55));
 			z.drawString(menuOptions[i], MarioPanel.WIDTH / 2 - 60 + i * 5, 250 + i * 100);
-		
 		}
-		
 	}
 	public void keyPressed(int k) {
 			if (k == KeyEvent.VK_DOWN) {
@@ -46,9 +37,15 @@ public class menuState extends gameState {
 					selection = menuOptions.length - 1;
 				}
 			}
-			
-			
-			
+			if (k == KeyEvent.VK_ENTER) {
+				if (selection == 0) {
+					System.out.println("> Game playing...");
+					gsm.states.push(new lvl1(gsm));
+				} else if (selection == 1) {
+					System.out.println("- Game quit expectedly");
+					System.exit(0);
+				}
+			}
 	}
 	public void keyReleased(int k) {
 			
